7e590fae7cf83d30909171d67d6f8a761ad6c230
FlorianB30 <biendine.florian@gmail.com>
2025-03-07 11:32:04 +0100

    some functions + struct + impl

---
+++ rust-bots/src/bot.rs
@@ -0,0 +1,38 @@
+pub struct Bot {
+    pub pos_x: i32,
+    pub pos_y: i32,
+    pub type_bot: BotType,
+    pub map_know: String
+}
+
+pub enum BotType {
+    Explorator,
+    CollectorEnergy,
+    CollectorMineral,
+    Scientist
+}
+
+impl Bot {
+    fn move_bot(&mut self, pos_x: i32, pos_y:i32) {
+        self.pos_x = pos_x;
+        self.pos_y = pos_y
+    }
+
+    fn obstacle(pos_x: i32, pos_y: i32) -> bool {
+        // verifier si il y a des obstacles près du bot
+        false
+    }
+
+    fn go_home(&mut self) {
+        // retour à la station
+    }
+
+    pub fn action(&mut self) {
+        match self.type_bot {
+            BotType::Explorator => {
+                println(self.map_know);
+            }
+            _ => println!("Type de bot inconnu."),
+        }
+    }
+}
--- rust-bots/src/main.rs
+++ rust-bots/src/main.rs
@@ -1,5 +1,14 @@
 mod map;
+mod bot;
 
 fn main() {
-    map::generate_map();
+    //map::generate_map();
+    let mut bot = bot::Bot {
+        pos_x: 0,
+        pos_y: 0,
+        type_bot: bot::BotType::Explorator,
+        map_know: "".to_string()
+    };
+
+    bot.action();
 }
