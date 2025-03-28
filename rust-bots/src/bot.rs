50a2440a21fa0e5bc28027c0062042cc1b38b8f0
FlorianB30 <96230889+FlorianB30@users.noreply.github.com>
2025-03-28 15:33:28 +0100

    bot file

--- rust-bots/src/bot.rs
+++ rust-bots/src/bot.rs
@@ -1,11 +1,24 @@
 use crate::map::Map; 
+use std::sync::mpsc;
+use std::thread;
+use std::sync::{Arc, Mutex};
+
+pub struct BotInfo {
+    pub x: usize,
+    pub y: usize,
+  //  pub map: Map,
+    pub msg: String,
+    pub ping: bool
+}
 
 pub struct Bot {
     pub pos_x: usize,
     pub pos_y: usize,
     pub type_bot: BotType,
     pub map_know: Map,
-    pub bag: i32
+    pub bag: i32,
+    pub tx: mpsc::Sender<BotInfo>,
+    pub rx: Arc<Mutex<mpsc::Receiver<BotInfo>>>,
 }
 
 pub enum BotType {
@@ -17,9 +30,27 @@ pub enum BotType {
 
 impl Bot {
     pub fn control(&mut self, pos_x: usize, pos_y:usize) {
+        self.communication();
         if self.is_on_map(pos_x, pos_y) {
             self.move_bot(pos_x, pos_y);
         }
+    }
+
+    fn communication(&self) {
+        let infos = BotInfo {
+            x: self.pos_x,
+            y: self.pos_y,
+            // map: self.map_know,
+            msg: "Test".to_string(),
+            ping: true
+        };
+        
+        let tx_clone = self.tx.clone();
+        let bot_thread = thread::spawn(move || {
+            tx_clone.send(infos).unwrap(); 
+        });
+
+        bot_thread.join().unwrap();
     }
 
     fn move_bot(&mut self, pos_x: usize, pos_y:usize) {
@@ -56,7 +87,7 @@ impl Bot {
     }
 
     fn is_on_map(&mut self, pos_x: usize, pos_y: usize) -> bool {
-        return if self.map_know.x >= pos_x && self.map_know.y >= pos_y {
+        return if self.map_know.width >= pos_x && self.map_know.height >= pos_y {
             true
         } else {
             false
