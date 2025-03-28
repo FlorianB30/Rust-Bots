6ee4adaa05502e2f895d45fd762ec74283f9cfda
FlorianB30 <96230889+FlorianB30@users.noreply.github.com>
2025-03-28 15:59:08 +0100

    add station on main

--- rust-bots/src/main.rs
+++ rust-bots/src/main.rs
@@ -7,21 +7,34 @@ fn main() {
     let mut map = map::Map::new(20, 10, 42); 
     
     map.display();
-    let positions = [(5, 5), (1, 1), (10, 3), (15, 7)];
-    for &(x, y) in &positions {
-        println!("\nEssai de collecte à la position ({}, {}):", x, y);
-        if let Some(cell) = map.get_cell(x, y) {
-            println!("Cellule à la position ({}, {}): {:?}", x, y, cell);
-        } else {
-            println!("Cellule invalide à la position ({}, {})", x, y);
-        }
 
-        if let Some(resource) = map.collect_resource(x, y) {
-            println!("Ressource collectée : {:?}", resource);
-        } else {
-            println!("Aucune ressource à collecter à cette position.");
-        }
-    }
+    let station = station::::new(    
+        bots: Vec<Bot>::new(),
+        pos_x: 0,
+        pos_y: 0,
+        pub map: map
+    )
+
+
+
+    // TESTS
+
+    // let positions = [(5, 5), (1, 1), (10, 3), (15, 7)];
+    // for &(x, y) in &positions {
+    //     println!("\nEssai de collecte à la position ({}, {}):", x, y);
+    //     if let Some(cell) = map.get_cell(x, y) {
+    //         println!("Cellule à la position ({}, {}): {:?}", x, y, cell);
+    //     } else {
+    //         println!("Cellule invalide à la position ({}, {})", x, y);
+    //     }
+
+    //     if let Some(resource) = map.collect_resource(x, y) {
+    //         println!("Ressource collectée : {:?}", resource);
+    //     } else {
+    //         println!("Aucune ressource à collecter à cette position.");
+    //     }
+    // }
+
     // let x = 5;
     // let y = 5;
 
