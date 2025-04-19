bb5110b3726a60bed2915b23001f97b80ddbff63
molka-az <molka.azaouzi@esprit.tn>
2025-04-19 21:53:40 +0200

    station et detection obstacle

--- rust-bots/src/map.rs
+++ rust-bots/src/map.rs
@@ -1,42 +1,65 @@
-use ansi_term::Colour;
+use noise::{NoiseFn, Perlin};
+use bevy::prelude::Resource;
 
+
+#[derive(Resource, Clone)]
 pub struct Map {
-    pub x:usize,
-    pub y:usize,
-    pub entire_map: Vec<Vec<String>>
+    pub width: usize,
+    pub height: usize,
+    pub seed: u32,
+    pub grid: Vec<Vec<Tile>>,
 }
 
+#[derive(Resource, Clone, Copy)]
+pub struct MapSize {
+    pub width: usize,
+    pub height: usize,
+}
+
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Tile {
+    Empty,
+    Obstacle,
+    Energy,
+    Mineral,
+    Science,
+    Station,
+    Bot,
+}
+
+
 impl Map {
-    pub fn generate_map(&mut self) {
-        let mut vec_y:Vec<Vec<String>> = Vec::new(); 
-        for _i in 0..self.y {  
-            let mut vec_x:Vec<String> = Vec::new();
-            for _j in 0..self.x {
-                vec_x.push(".".to_string());
-            }
-            vec_y.push(vec_x);
+    pub fn new(width: usize, height: usize, seed: u32) -> Self {
+        Self {
+            width,
+            height,
+            seed,
+            grid: vec![vec![Tile::Empty; width]; height],
         }
-        self.entire_map = vec_y;
     }
 
-    pub fn display_map(&mut self) {
-        println!("");
-        for y in &self.entire_map {  
-            for x in y {
-                match x.as_str() {
-                    "S" => {
-                        print!("{}", Colour::Green.paint("S"));
+    pub fn generate(&mut self) {
+        let noise = Perlin::new(self.seed);
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let val = noise.get([x as f64 / 10.0, y as f64 / 10.0]);
+                self.grid[y][x] = if val < -0.4 {
+                    Tile::Obstacle
+                } else if val > 0.5 {
+                    match (x + y) % 3 {
+                        0 => Tile::Energy,
+                        1 => Tile::Mineral,
+                        _ => Tile::Science,
                     }
-                    "O" => {
-                        print!("{}", Colour::Green.paint("O"));
-                    }
-                    _ => {
-                        print!("{}", Colour::Fixed(130).paint("."));
-                    }
-                }
+                } else {
+                    Tile::Empty
+                };
             }
-            println!("");
         }
     }
-    
+
+    pub fn is_valid(&self, x: usize, y: usize) -> bool {
+        x < self.width && y < self.height && !matches!(self.grid[y][x], Tile::Obstacle)
+    }
 }
